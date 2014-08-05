3f8aad326264b159734fd248a693dbe843f10fea
Iddah <iddahkamau@gmail.com>
2014-08-05 17:13:06 +0300

    informea .NET version 1

---
+++ informeaOData/informea.svc.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Data.Services
+Imports System.Data.Services.Common
+Imports System.Linq
+Imports System.ServiceModel.Web
+Imports System.Data.Services.Providers
+
+Public Class informea
+    Inherits EntityFrameworkDataService(Of InformeaEntities)
+
+    Public Shared Sub InitializeService(ByVal config As DataServiceConfiguration)
+        config.SetEntitySetAccessRule("*", EntitySetRights.AllRead)
+        config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3
+        config.SetEntitySetPageSize("*", 200)
+        config.UseVerboseErrors = True
+
+    End Sub
+
+End Class
